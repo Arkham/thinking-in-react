81ee3fa5cba01b7480ebcb675740bcb662314a72
Ju Liu <ju.liu@welaika.com>
2015-03-22 19:46:03 +0000

    Initial commit

---
+++ public/thinking.js
@@ -0,0 +1,9 @@
+var FilterableProductTable = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+});
+
+React.render(<FilterableProductTable url="products.json" />, document.body);
